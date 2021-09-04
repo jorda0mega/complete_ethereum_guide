be55c5221807f2f7292dcf15c36ae440cf623a4d
Jorge David <484084+jorda0mega@users.noreply.github.com>
2021-09-04 18:24:16 -0400

    initial template code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+import { useState } from 'react';
+import { ethers } from 'hardhat';
+import { Contract } from 'ethers';
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
+  const [greeting, setGreetingValue] = useState();
+
+  async function requestAccount(){
+    await window.ethereum.request({method: 'eth_requestAccounts'});
+  }
+
+  async function fetchGreeting(){
+    if (typeof window.ethereum !== 'undefined'){
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+
+      try {
+        const data = await contract.greet();
+        console.log('data: ', data);
+      } catch(err) {
+        console.log("Error: ", err);
+      }
+    }
+  }
+
+  async function setGreeting(greeting){
+    if(!greeting) return;
+    if (typeof window.ethereum !== 'undefined'){
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
